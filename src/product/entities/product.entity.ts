c70854b805aeb4d3640f2013ff7f0b00dc896465
guillsou <35000004+GuillaumeGSO@users.noreply.github.com>
2024-12-21 10:48:29 +0800

    Refactor ProductEntity to use PrimaryGeneratedColumn and add unique index on productcode and location

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,15 +1,22 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { Column, Entity, Index, PrimaryGeneratedColumn, Unique } from 'typeorm';
 
 @Entity('products')
+@Unique(['productcode', 'location'])
+@Index(['productcode', 'location'])
 export class ProductEntity {
-  @PrimaryColumn({ type: 'varchar', length: 30 })
+  @PrimaryGeneratedColumn()
+  @Exclude()
+  id: number;
+
+  @Column({ type: 'varchar', length: 30 })
   productcode: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  location: string;
 
   @Column({ type: 'varchar', length: 300 })
   productdesc: string;
-
-  @PrimaryColumn({ type: 'varchar', length: 50 })
-  location: string;
 
   @Column({ type: 'decimal', precision: 12, scale: 2 })
   price: number;
